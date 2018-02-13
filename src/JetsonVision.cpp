525e8d243a3c135b8be77663bdd9b5724051124b
PB020 <abe05081995@gmail.com>
2018-02-13 16:12:40 -0500

    Finished, Need to Test

--- src/JetsonVision.cpp
+++ src/JetsonVision.cpp
@@ -7,30 +7,9 @@ using namespace std;
 using namespace cv;
 
 namespace {
-    /**
-     * Board creation Variables
-     */
-    const char* about_bc = "Create a ChArUco board image";
-    const char* keys_bc  =
-            "{@outfile |<none> | Output image }"
+    const char* about = "Pose estimation using a ChArUco board";
+    const char* keys  =
                     "{w        |       | Number of squares in X direction }"
-                    "{h        |       | Number of squares in Y direction }"
-                    "{sl       |       | Square side length (in pixels) }"
-                    "{ml       |       | Marker side length (in pixels) }"
-                    "{d        |       | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
-                    "DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, "
-                    "DICT_6X6_50=8, DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12,"
-                    "DICT_7X7_100=13, DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16}"
-                    "{m        |       | Margins size (in pixels). Default is (squareLength-markerLength) }"
-                    "{bb       | 1     | Number of bits in marker borders }"
-                    "{si       | false | show generated image }";
-
-    /**
-     * Board Detection Variables
-     */
-    const char* about_bd = "Pose estimation using a ChArUco board";
-    const char* keys_bd  =
-            "{w        |       | Number of squares in X direction }"
                     "{h        |       | Number of squares in Y direction }"
                     "{sl       |       | Square side length (in meters) }"
                     "{ml       |       | Marker side length (in meters) }"
@@ -46,79 +25,6 @@ namespace {
                     "{r        |       | show rejected candidates too }";
 }
 
-int main(int argc, char **argv[]){
-    cout << "Creating ChArUco Board" << endl;
-    int createBoard(argc, argv);
-    cout << "Board Created, Detecting Board" << endl;
-    int detectBoard(argc, argv);
-}
-
-/**
- * ChArUco Board Creation Method Section - OpenCV
- * @param argc
- * @param argv
- * @return
- */
-int createBoard(int argc, char **argv[]) {
-    CommandLineParser parser(argc, reinterpret_cast<const char *const *>(argv), keys_bc);
-    parser.about(about_bc);
-
-    if(argc < 7) {
-        parser.printMessage();
-        return 0;
-    }
-
-    int squaresX = parser.get<int>("w");
-    int squaresY = parser.get<int>("h");
-    int squareLength = parser.get<int>("sl");
-    int markerLength = parser.get<int>("ml");
-    int dictionaryId = parser.get<int>("d");
-    int margins = squareLength - markerLength;
-    if(parser.has("m")) {
-        margins = parser.get<int>("m");
-    }
-
-    int borderBits = parser.get<int>("bb");
-    bool showImage = parser.get<bool>("si");
-
-    String out = parser.get<String>(0);
-
-    if(!parser.check()) {
-        parser.printErrors();
-        return 0;
-    }
-
-    Ptr<aruco::Dictionary> dictionary =
-            aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
-
-    Size imageSize;
-    imageSize.width = squaresX * squareLength + 2 * margins;
-    imageSize.height = squaresY * squareLength + 2 * margins;
-
-    Ptr<aruco::CharucoBoard> board = aruco::CharucoBoard::create(squaresX, squaresY, (float)squareLength,
-                                                                 (float)markerLength, dictionary);
-
-    // show created board
-    Mat boardImage;
-    board->draw(imageSize, boardImage, margins, borderBits);
-
-    if(showImage) {
-        imshow("board", boardImage);
-        waitKey(0);
-    }
-
-    imwrite(out, boardImage);
-
-    return 0;
-}
-
-/**
- * ChArUco Board Detection Section - OpenCV
- * @param filename
- * @param camMatrix
- * @param distCoeffs
- * @return
- */
 static bool readCameraParameters(string filename, Mat &camMatrix, Mat &distCoeffs) {
     FileStorage fs(filename, FileStorage::READ);
     if(!fs.isOpened())
@@ -128,9 +34,6 @@ static bool readCameraParameters(string filename, Mat &camMatrix, Mat &distCoeff
     return true;
 }
 
-
-/**
- */
 static bool readDetectorParameters(string filename, Ptr<aruco::DetectorParameters> &params) {
     FileStorage fs(filename, FileStorage::READ);
     if(!fs.isOpened())
@@ -158,10 +61,9 @@ static bool readDetectorParameters(string filename, Ptr<aruco::DetectorParameter
     return true;
 }
 
-
-int detectBoard(int argc, char *argv[]) {
-    CommandLineParser parser(argc, reinterpret_cast<const char *const *>(argv), keys_bd);
-    parser.about(about_bd);
+int main(int argc, char *argv[]) {
+    CommandLineParser parser(argc, argv, keys);
+    parser.about(about);
 
     if(argc < 6) {
         parser.printMessage();
